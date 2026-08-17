7f5ae337eea6884426aa55a75adb6b7826912f44
Pavan gonuguntla <pavangonuguntla@macbookpro.mynetworksettings.com>
2026-08-17 10:52:56 -0500

    🔥 Sync latest Firebase bundle to main root

---
+++ sw.js
@@ -0,0 +1,32 @@
+// AKHIL 360 Studio Service Worker - Firebase Realtime Active
+const CACHE_NAME = 'akhil-360-v2-firebase';
+
+self.addEventListener('install', (event) => {
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) => {
+      return Promise.all(keys.map((k) => caches.delete(k)));
+    }).then(() => self.clients.claim())
+  );
+});
+
+self.addEventListener('notificationclick', (event) => {
+  event.notification.close();
+  event.waitUntil(
+    clients.matchAll({ type: 'window', includeUncontrolled: true }).then((clientList) => {
+      if (clientList.length > 0) {
+        let client = clientList[0];
+        for (let i = 0; i < clientList.length; i++) {
+          if (clientList[i].focused) {
+            client = clientList[i];
+          }
+        }
+        return client.focus();
+      }
+      return clients.openWindow('./');
+    })
+  );
+});
